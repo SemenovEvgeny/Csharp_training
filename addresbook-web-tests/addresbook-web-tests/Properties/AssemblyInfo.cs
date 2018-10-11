35be1cb899e59fff069d42b86f186b60c8a6c8fa
Евгений Семенов <semenov.evgenij@gmail.com>
2018-10-11 12:21:41 +0300

    пустой тест

---
+++ addresbook-web-tests/addresbook-web-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addresbook-web-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Страховой дом ВСК")]
+[assembly: AssemblyProduct("addresbook-web-tests")]
+[assembly: AssemblyCopyright("Copyright © Страховой дом ВСК 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06988435-4f28-485e-9e6f-ebcc7cb6b1ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
