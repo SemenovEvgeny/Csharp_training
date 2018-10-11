2184f33a22b659f8b633e381b17996de3869a80b
Евгений Семенов <semenov.evgenij@gmail.com>
2018-10-11 17:09:18 +0300

    1

---
+++ addresbook-web-tests/addresbook-web-tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addresbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.getSize(), 5);
+            Assert.AreEqual(s2.getSize(), 10);
+            Assert.AreEqual(s3.getSize(), 5);
+
+            s3.setSize(15);
+            Assert.AreEqual(s1.getSize(), 15);
+        }
+    }
+}
